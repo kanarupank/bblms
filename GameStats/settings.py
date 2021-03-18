4f341dac56f2a9466390a5b58a451e43070d2566
Kanarupan <kanarupank.20@uom.lk>
2021-03-18 16:45:14 +0530

    SWAGGER setup

--- GameStats/settings.py
+++ GameStats/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'bblms',
     'django_seed',
     'rest_framework',
+    'drf_yasg',
 
 ]
 
--- bblms/urls.py
+++ bblms/urls.py
@@ -1,10 +1,24 @@
+from django.conf.urls import url
 from django.urls import include, path
 from rest_framework import routers
-
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from .views import GenericTeamAPIView, GenericPlayerAPIView, TeamStatsView, GamesView, TopPlayersView, UserSessionView
 
 router = routers.DefaultRouter()
-
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 # Wire up API using automatic URL routing.
 # Additionally, include login URLs for the browsable API.
 urlpatterns = [
@@ -18,5 +32,8 @@ urlpatterns = [
     path('team/stats/<int:team_id>', TeamStatsView.as_view()),
     path('games/', GamesView.as_view()),
     path('login/', include('rest_framework.urls', namespace='rest_framework')),
-    path('sessions/', UserSessionView.as_view())
+    path('sessions/', UserSessionView.as_view()),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
